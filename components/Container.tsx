6da6eb2f6633d7109ff16c3f4f5ac98561a12e74
Sampaio Leal <samp14hd@gmail.com>
2021-09-09 22:17:16 -0300

    feat: added container for consistant design

---
+++ components/Container.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: ${({ theme }) => theme.spacing(3)}px;
+  max-width: 1280px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+export default Container;
